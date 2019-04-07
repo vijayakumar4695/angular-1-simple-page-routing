a84201f2b0760976b57fad5941da150037903298
vijayakumar4695 <vjvijayakumar4695@gmail.com>
2019-04-07 18:58:34 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { ServiceComponent } from './service/service.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    ServiceComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+       {path:'',pathMatch:'full',redirectTo:'home'},
+       {path:'home',component:HomeComponent},
+        {path:'about',component:AboutUsComponent},
+        {path:'contact',component:ContactUsComponent},
+        {path:'service',component:ServiceComponent}
+      
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
